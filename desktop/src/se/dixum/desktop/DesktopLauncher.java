6c1bde28ad69a13d3330e19fc7007d00a377b91b
Andreas Brommund <andreas@brommund.se>
2015-12-12 13:28:06 +0100

    Add: Libgdx

---
+++ desktop/src/se/dixum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.dixum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.dixum.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
