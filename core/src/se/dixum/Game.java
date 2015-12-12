6b242a05d056346190b8ac43ba6b61c5f3066ef2
Andreas Brommund <andreas@brommund.se>
2015-12-12 15:02:47 +0100

    Tweak: Reader a triangle

--- core/src/se/dixum/Game.java
+++ core/src/se/dixum/Game.java
@@ -3,25 +3,32 @@ package se.dixum;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import se.dixum.connection.SocketConnection;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    ShapeRenderer shapeRenderer;
+    SocketConnection connection;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        shapeRenderer = new ShapeRenderer();
+        connection = new SocketConnection("192.168.1.31",7978);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+        update();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.identity();
+        shapeRenderer.setColor(1,1,1,1);
+        shapeRenderer.triangle(0,0,100,100,100,0);
+		shapeRenderer.end();
 	}
+
+    public void update(){
+
+    }
 }
