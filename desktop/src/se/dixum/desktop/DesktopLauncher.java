28430624329662ef53d2f901df4e21d7f857c244
Andreas Brommund <andreas@brommund.se>
2015-12-14 11:37:29 +0100

    Set res

--- desktop/src/se/dixum/desktop/DesktopLauncher.java
+++ desktop/src/se/dixum/desktop/DesktopLauncher.java
@@ -2,12 +2,15 @@ package se.dixum.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import se.dixum.sprite.Game;
+import se.dixum.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.samples = 2;
+
+		config.width = 1280;
+		config.height = 720;
 
 		new LwjglApplication(new Game(), config);
 	}
